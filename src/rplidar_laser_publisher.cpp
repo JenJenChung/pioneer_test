6eb848f75860acec355b8ed7d0c26b498c8368f7
rebhuhnc <rebhuhnc@oregonstate.edu>
2017-04-12 17:43:27 -0700

    Corrected transform tree to include pioneer urdf model

--- src/rplidar_laser_publisher.cpp
+++ src/rplidar_laser_publisher.cpp
@@ -7,7 +7,7 @@ void sonarPC2Callback(const sensor_msgs::LaserScan& msg)
 {
   sensor_msgs::LaserScan newscan = msg ;
   newscan.header.stamp = ros::Time::now() ;
-  newscan.header.frame_id = "laser" ;
+  newscan.header.frame_id = "rp_laser" ;
   
   pub.publish(newscan) ;
 }
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "rplidar_laser_publisher");
 
   ros::NodeHandle nHandpub;
-  pub = nHandpub.advertise<sensor_msgs::LaserScan>("/rplidar_scan", 10);
+  pub = nHandpub.advertise<sensor_msgs::LaserScan>("/base_scan", 10);
   
   ros::NodeHandle nHandsub;  
   ros::Subscriber sub = nHandsub.subscribe("/scan", 10, &sonarPC2Callback);
